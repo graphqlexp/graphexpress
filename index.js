e035524b8029ee9ee04ac7d21536aec7c9a86e2a
pujie <pw.prayitno@gmail.com>
2017-11-09 05:10:41 +0700

    first files

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+graphqlHTTP = require('express-graphql'),
+{buildSchema} = require('graphql'),
+schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`),
+root = {hello:()=>'Gello Woo'},
+app = express();
+app.use('/graphql',graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000,()=>console.log('Silakan browse ke localhost:4000/graphql'));
